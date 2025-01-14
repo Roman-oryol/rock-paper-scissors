45535324274a19da65adfa288519e0fc8ad2124b
Roman Oryol <roman.oryol83@gmail.com>
2025-01-14 18:07:22 +0200

    feat: add reset functionality and improve game result display

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const playerChoiceButtons = document.querySelectorAll('button');
+const playerChoiceButtons = document.querySelectorAll('button:not(#reset)');
 const resultElement = document.querySelector('#result');
+const resetBtn = document.querySelector('#reset');
+const titleElement = document.querySelector('h2');
+
+const WINNING_SCORE = 5;
 
 let humanScore = 0;
 let computerScore = 0;
+
+resetBtn.style.display = 'none';
+resetBtn.addEventListener('click', reset);
+
+function reset() {
+  playerChoiceButtons.forEach((button) => {
+    button.disabled = false;
+  });
+  humanScore = 0;
+  computerScore = 0;
+  resetBtn.style.display = 'none';
+  titleElement.innerHTML = `Please make your choice.<br /> The first to reach 5 points wins!`;
+  resultElement.innerHTML = '';
+}
 
 playerChoiceButtons.forEach((button) => {
   button.addEventListener('click', handlePlayerChoice);
@@ -30,8 +48,38 @@ function getHumanChoice(evt) {
   return evt.target.id;
 }
 
+function displayResult(
+  humanScore,
+  humanChoice,
+  computerScore,
+  computerChoice,
+  isHumanWinner
+) {
+  let message = `Round result:
+  You chose ${humanChoice},
+  Computer chose ${computerChoice}.<br>
+  You ${isHumanWinner ? 'win' : 'lose'}!<br>
+  Current score: ${humanScore} - ${computerScore}`;
+  let isGameOver =
+    humanScore === WINNING_SCORE || computerScore === WINNING_SCORE;
+
+  if (isGameOver) {
+    const isHumanWinner = humanScore === WINNING_SCORE;
+    message = `${isHumanWinner ? 'You win' : 'Computer wins'} the game
+    with a score of ${isHumanWinner ? humanScore : computerScore} : ${
+      isHumanWinner ? computerScore : humanScore
+    }!`;
+    playerChoiceButtons.forEach((button) => {
+      button.disabled = true;
+    });
+    resetBtn.style.display = 'block';
+    titleElement.textContent = 'Game over!';
+  }
+
+  resultElement.innerHTML = message;
+}
+
 function playRound(humanChoice, computerChoice) {
-  let message = '';
   const isHumanWinner =
     (humanChoice === 'rock' && computerChoice === 'scissors') ||
     (humanChoice === 'scissors' && computerChoice === 'paper') ||
@@ -41,15 +89,18 @@ function playRound(humanChoice, computerChoice) {
   computerChoice = computerChoice.toLowerCase();
 
   if (isHumanWinner) {
-    message = `You win! ${humanChoice} beats ${computerChoice}.`;
     humanScore++;
-  } else if (humanChoice === computerChoice) {
-    message = "It's a draw!";
-  } else {
-    message = `You lose! ${computerChoice} beats ${humanChoice}.`;
+  }
+
+  if (!isHumanWinner) {
     computerScore++;
   }
 
-  alert(`${message}
-Score: ${humanScore} : ${computerScore}`);
+  displayResult(
+    humanScore,
+    humanChoice,
+    computerScore,
+    computerChoice,
+    isHumanWinner
+  );
 }
